5a7b5e774feec93125d776a8dade8f33efcd7d23
Ekmcleod <mcleodekaterina@gmail.com>
2024-05-02 13:29:09 -0700

    reflection

--- lab7/lab.js
+++ lab7/lab.js
@@ -2,11 +2,33 @@
 // Author: Ekaterina Mcleod and Tanner Norriss-Spano
 // Date: 05/02/2024
 
-function sortUserName () {
-    var userName = window.prompt("Hi, please type in your name.");
-    console.log ("userName = ", userName);
+//function sortUserName () {
+    //var userName = window.prompt("Hi, please type in your name.");
+    //console.log ("userName = ", userName);
 
-    var nameArray = userName.split('');
+    //var nameArray = userName.split('');
+   // console.log("nameArray =", nameArray);
+
+   // var nameArraySort = nameArray.sort();
+    //console.log("nameArraySort= ", nameArraySort);
+
+   // var nameSorted = nameArraySort.join('');
+    //console.log("nameSorted=", nameSorted);
+
+    //return nameSorted
+
+//}
+
+//document.writeln("Here I fixed your name by the way: ", sortUserName(), "</br");
+
+
+//new function that uses sort ()
+
+function sortUserName() {
+    var userName = window.prompt("Hi, please type in your name.");
+    console.log("userName = ", userName);
+
+    var nameArray = userName.toLowerCase().split('');
     console.log("nameArray =", nameArray);
 
     var nameArraySort = nameArray.sort();
@@ -15,8 +37,8 @@ function sortUserName () {
     var nameSorted = nameArraySort.join('');
     console.log("nameSorted=", nameSorted);
 
-    return nameSorted
-
+    return nameSorted;
 }
 
-document.writeln("Here I fixed your name by the way: ", sortUserName(), "</br");
+document.writeln("Here I fixed your name by the way: ", sortUserName(), "</br>");
+
